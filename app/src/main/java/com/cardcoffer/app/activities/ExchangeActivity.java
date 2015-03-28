212a584af24a89ba6d93cc1e3e1db6856b06499e
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-03-29 03:34:01 +0430

    Implemented Receiving layout for Exchange Activity. changed the exchange button icons to distinguish between sending/receiving

--- app/src/main/java/com/cardcoffer/app/activities/ExchangeActivity.java
+++ app/src/main/java/com/cardcoffer/app/activities/ExchangeActivity.java
@@ -4,16 +4,65 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.cardcoffer.app.R;
 
 public class ExchangeActivity extends Activity {
+
+
+    ViewFlipper viewFlipper;
+    TextView sendBig,sendTap,receiveBig,receiveTap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
         getActionBar().setTitle("Exchange Room");
+
+        viewFlipper = (ViewFlipper) findViewById(R.id.exchange_viewFlipper);
+
+        sendBig = (TextView) findViewById(R.id.tvExchange_sending);
+        sendTap = (TextView) findViewById(R.id.tvExchange_tapToReceive);
+
+        receiveBig = (TextView) findViewById(R.id.tvExchange_receiving);
+        receiveTap = (TextView) findViewById(R.id.tvExchange_tapToSend);
+
+        sendBig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewFlipper.showNext();
+
+            }
+        });
+
+        sendTap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewFlipper.showNext();
+
+            }
+        });
+        receiveBig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewFlipper.showNext();
+
+            }
+        });
+        receiveTap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewFlipper.showNext();
+
+            }
+        });
 
     }
 
