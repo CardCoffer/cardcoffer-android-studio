9512cf5c1e4f48d7045531e5593f870d081f29dd
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-03-28 11:51:58 +0430

    Updated dazzler's library. improved home layout.

--- app/src/main/java/com/cardcoffer/app/activities/HomeActivity.java
+++ app/src/main/java/com/cardcoffer/app/activities/HomeActivity.java
@@ -4,53 +4,56 @@ import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
 
 import com.cardcoffer.app.R;
 import com.cardcoffer.app.R.id;
-import com.cardcoffer.app.R.layout;
-import com.cardcoffer.app.R.menu;
 import com.cardcoffer.app.customviews.ItemCardThumbnail;
 import com.parse.ParseUser;
 import com.thedazzler.droidicon.IconicFontDrawable;
 import com.thedazzler.droidicon.badges.DroidiconBadge;
-import com.thedazzler.droidicon.util.TypefaceManager;
+import com.thedazzler.droidicon.typeface.CustomTypefaceHolder;
+import com.thedazzler.droidicon.typeface.TypefaceManager;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
+ *
  * @author sinash
  *
  */
 public class HomeActivity extends Activity {
 
-	LinearLayout llCardContainer;
-	ImageButton btnCardCoffer;
+    LinearLayout llCardContainer;
+    ImageButton btnCardCoffer;
     DroidiconBadge btnExchange, btnCheckIn;
+    ImageView btnTest;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
-		llCardContainer = (LinearLayout) findViewById(R.id.home_llCardContainer);
+        llCardContainer = (LinearLayout) findViewById(R.id.home_llCardContainer);
 
-
-        initCustomIcons();
-
-		btnCardCoffer = (ImageButton) findViewById(R.id.btnHome_cardCoffer);
-		btnCheckIn = (DroidiconBadge) findViewById(R.id.btnHome_checkIn);
+//        initCustomIcons();
 
 
-		//btnExchange = (ImageButton) findViewById(R.id.btnHome_exchange);
+
+        btnCardCoffer = (ImageButton) findViewById(R.id.btnHome_cardCoffer);
+        btnCheckIn = (DroidiconBadge) findViewById(R.id.btnHome_checkIn);
+
+
+        //btnExchange = (ImageButton) findViewById(R.id.btnHome_exchange);
         btnExchange = (DroidiconBadge) findViewById(id.btnHome_exchange);
 
         btnExchange.setOnClickListener(new OnClickListener() {
@@ -61,89 +64,109 @@ public class HomeActivity extends Activity {
             }
         });
 
-		
-		btnCheckIn.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				launchCheckInActivity();
-				
-			}
-		});
-		
-		btnCardCoffer.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				launchStacksActivity();
-				
-			}
-		});
-		
-		if(ParseUser.getCurrentUser() == null){
-			
-			launchLoginActivity();
-			
-		}
 
-	}
+        btnCheckIn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                launchCheckInActivity();
+
+            }
+        });
+
+        btnCardCoffer.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                launchStacksActivity();
+
+            }
+        });
+
+        if(ParseUser.getCurrentUser() == null){
+
+            launchLoginActivity();
+
+        }
+
+    }
 
     private void initCustomIcons() {
+
+
+////Init your icons name-val mapping
+//        Map<String, Integer> customMap = new HashMap<String, Integer>();
+//        customMap.put("custom", 0xe600);
+//
+///**Create a CustomTypefaceHolder
+//* The params in the constructor are:
+//*  - Prefix name: the prefix you would like to use, make sure this one matches whatever you have in your name-val mapping.
+//*  - The font file resource: e.g. Your font file is under res/raw/custom.ttf then put 'R.raw.custom' here.
+//*  - Name-val map: the name-val mapping you created before.
+//**/
+//        CustomTypefaceHolder customTypefaceHolder = new CustomTypefaceHolder("custom", R.raw.custom, customMap);
+//
+////Add it to TypefaceManager.
+//        TypefaceManager.getInstance().addNewTypefaceHolder(customTypefaceHolder);
+//
+////That's it! Use it in your app
+//
+
 
     }
 
     protected void launchStacksActivity() {
-		Intent intent = new Intent(this, StacksActivity.class);
-		startActivity(intent);
-	}
+        Intent intent = new Intent(this, StacksActivity.class);
+        startActivity(intent);
+    }
 
-	private void launchLoginActivity() {
-		
-		Intent intent = new Intent(this, LoginActivity.class);
-		startActivity(intent);
-		finish();
-		
-	}
+    private void launchLoginActivity() {
 
-	protected void launchCheckInActivity() {
-		
-		Intent intent = new Intent(this, CheckInActivity.class);
-		startActivity(intent);
-		
-	}
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+    }
 
-		getMenuInflater().inflate(R.menu.main_actionbar, menu);
+    protected void launchCheckInActivity() {
 
-		// actionbar searchview animation
+        Intent intent = new Intent(this, CheckInActivity.class);
+        startActivity(intent);
 
-		SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-		int searchBarId = searchView.getContext().getResources().getIdentifier("android:id/search_bar", null, null);
-		LinearLayout searchBar = (LinearLayout) searchView.findViewById(searchBarId);
-		searchBar.setLayoutTransition(new LayoutTransition()); // TODO set good
-																// animation!
+    }
 
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		if (id == R.id.action_search) {
+        getMenuInflater().inflate(R.menu.main_actionbar, menu);
 
-			llCardContainer.addView(new ItemCardThumbnail(this));
+        // actionbar searchview animation
 
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        int searchBarId = searchView.getContext().getResources().getIdentifier("android:id/search_bar", null, null);
+        LinearLayout searchBar = (LinearLayout) searchView.findViewById(searchBarId);
+        searchBar.setLayoutTransition(new LayoutTransition()); // TODO set good
+        // animation!
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_search) {
+
+            llCardContainer.addView(new ItemCardThumbnail(this));
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
