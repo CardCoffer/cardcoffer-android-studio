49ee4316dd27c7ef42fb4e4ae170b47a6f513e81
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-03-31 20:18:40 +0430

    Constraints added for Adding New Cards.

--- app/src/main/java/com/cardcoffer/app/activities/AddNewCardActivity.java
+++ app/src/main/java/com/cardcoffer/app/activities/AddNewCardActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import com.cardcoffer.app.R;
@@ -14,6 +16,8 @@ public class AddNewCardActivity extends Activity {
 
     private Button btnSaveSync;
     private ViewFlipper viewFlipper;
+    private EditText etName, etJobTitle, etAffiliation, etAddress, etPhone, etEmail, etFacebook, etTwitter, etLinkedin, etGoogleplus, etMiniResume;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +28,52 @@ public class AddNewCardActivity extends Activity {
         btnSaveSync = (Button) findViewById(R.id.btnAddNewCard_saveChanges);
         viewFlipper = (ViewFlipper) findViewById(R.id.addnewcard_viewFlipper);
 
+        etName = (EditText) findViewById(R.id.addnewcard_etName);
+        etJobTitle = (EditText) findViewById(R.id.addnewcard_etJobTitle);
+        etAffiliation = (EditText) findViewById(R.id.addnewcard_etAffiliation);
+        etAddress = (EditText) findViewById(R.id.addnewcard_etAddress);
+        etPhone = (EditText) findViewById(R.id.addnewcard_etPhone);
+        etEmail = (EditText) findViewById(R.id.addnewcard_etEmail);
+        etFacebook = (EditText) findViewById(R.id.addnewcard_etFacebook);
+        etTwitter = (EditText) findViewById(R.id.addnewcard_etTwitter);
+        etLinkedin = (EditText) findViewById(R.id.addnewcard_etLinkedin);
+        etGoogleplus = (EditText) findViewById(R.id.addnewcard_etGoogleplus);
+        etMiniResume = (EditText) findViewById(R.id.addnewcard_etMiniResume);
+
+
+
         btnSaveSync.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                viewFlipper.setDisplayedChild(1);
+                if(detailsAreProvided()) {
 
+                    saveCardToCloud();
+                    viewFlipper.setDisplayedChild(1);
+
+                }
 
             }
         });
+    }
+
+    private boolean detailsAreProvided() {
+
+        if(etName.getText().toString().equals("") || etJobTitle.getText().toString().equals("") || etAffiliation.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(), "Please fill in the first three fields.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else
+            return true;
+
+    }
+
+    private void saveCardToCloud() {
+
+
+
+
+
     }
 
 
