d9cacf40933c7255e7a7b16c074a4a7432966316
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-03-31 19:21:38 +0430

    Improved AddNewCard layout and added Progress bar.

--- app/src/main/java/com/cardcoffer/app/activities/AddNewCardActivity.java
+++ app/src/main/java/com/cardcoffer/app/activities/AddNewCardActivity.java
@@ -4,16 +4,35 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 import com.cardcoffer.app.R;
 
 public class AddNewCardActivity extends Activity {
+
+    private Button btnSaveSync;
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_card);
         getActionBar().setTitle("Add New Card");
+
+        btnSaveSync = (Button) findViewById(R.id.btnAddNewCard_saveChanges);
+        viewFlipper = (ViewFlipper) findViewById(R.id.addnewcard_viewFlipper);
+
+        btnSaveSync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                viewFlipper.setDisplayedChild(1);
+
+
+            }
+        });
     }
 
 
