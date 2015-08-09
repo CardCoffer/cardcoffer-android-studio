3180b96cc7de6358bca0fc494f74241af01d17b0
Sina Sheikholeslami <ssheikholeslami@live.com>
2015-08-10 02:59:16 +0430

    + ‌‌Bluetooth Turning On & Enabling Discovery

--- app/src/main/java/com/cardcoffer/app/activities/BluetoothActivity.java
+++ app/src/main/java/com/cardcoffer/app/activities/BluetoothActivity.java
@@ -2,14 +2,24 @@ package com.cardcoffer.app.activities;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ViewFlipper;
 
 import com.cardcoffer.app.R;
 
+
+//TODO listen for bluetooth state changes , so that users can't meddle with the app! :D
+//use ACTION_STATE_CHANGED broadcast intent
 public class BluetoothActivity extends Activity {
 
+    ViewFlipper viewFlipper;
+
+
+    //request codes
+    int REQUEST_ENABLE_BT = 1001;
 
     BluetoothAdapter bluetoothAdapter;
     @Override
@@ -17,6 +27,7 @@ public class BluetoothActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
 
+        viewFlipper = (ViewFlipper) findViewById(R.id.bluetooth_viewFlipper);
 
 
         bluetoothAdapter  = BluetoothAdapter.getDefaultAdapter();
@@ -24,6 +35,22 @@ public class BluetoothActivity extends Activity {
         if(bluetoothAdapter == null){
             //device doesn't support bluetooth
 
+            viewFlipper.setDisplayedChild(0);
+            finish();
+            return; // FIXME ??? :D
+        }
+
+        viewFlipper.setDisplayedChild(1);
+
+        if (!bluetoothAdapter.isEnabled()){
+
+            //Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            //startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0);
+            startActivity(discoverableIntent);
         }
     }
 
